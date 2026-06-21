9de3c80986f86828f1eac1ccafb01351000261b8
Rohith <sairohithn@gmail.com>
2026-06-21 15:46:04 +0530

    Two Sum

---
+++ src/com/rohith/dsa/arrays/TwoSum.java
@@ -0,0 +1,25 @@
+package com.rohith.dsa.arrays;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> result = new HashMap<>();
+        for(int i = 0; i<nums.length ; i++){
+            int diff = target-nums[i];
+            if(result.containsKey(diff)){
+                return new int[] {result.get(diff),i};
+            }
+            result.put(nums[i],i);
+        }
+        return new int[] {};
+    }
+    public static void main(String[] args){
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        TwoSum solution = new TwoSum();
+        int[] answer = solution.twoSum(nums, target);
+        System.out.println(Arrays.toString(answer));
+    }
+}
